ba646f32413ae7fdd67b3e509d486c2b426beda6
Ankit <urstrulyankit@protonmail.com>
2025-01-16 14:09:25 +0530

    live lecture

--- react-redux/src/components/TodoInput.jsx
+++ react-redux/src/components/TodoInput.jsx
@@ -4,27 +4,27 @@ import { v4 as uuid } from "uuid";
 import { addTodo } from "../redux/action";
 
 export const TodoInput = () => {
-  const [title, settitle] = useState("");
-  const dispatch = useDispatch();
+   const [title, settitle] = useState("");
+   const dispatch = useDispatch();
 
-  const handleAdd = () => {
-    const payload = {
-      title,
-      status: false,
-      id: uuid()
-    };
-    dispatch(addTodo(payload));
-  };
+   const handleAdd = () => {
+      const payload = {
+         title,
+         status: false,
+         id: uuid()
+      };
+      dispatch(addTodo(payload));
+   };
 
-  return (
-    <div>
-      <h3>Add Todo</h3>
-      <input
-        value={title}
-        placeholder="Add todo"
-        onChange={(e) => settitle(e.target.value)}
-      />
-      <button onClick={handleAdd}>Add todo</button>
-    </div>
-  );
+   return (
+      <div>
+         <h3>Add Todo</h3>
+         <input
+            value={title}
+            placeholder="Add todo"
+            onChange={(e) => settitle(e.target.value)}
+         />
+         <button onClick={handleAdd}>Add todo</button>
+      </div>
+   );
 };
